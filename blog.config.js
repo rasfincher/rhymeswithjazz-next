89718b194b59044efb8ce2924de82f3710d98a50
Ras Fincher <ras@rasfincher.com>
2023-05-19 21:46:57 -0500

    Added avatar

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: "https://og-image.vercel.app", // The link to generate OG image, don't end with a slash
-  socialLink: "https://vmst.io/rhymeswithjazz",
+  socialLink: "https://vmst.io/@rhymeswithjazz",
   seo: {
     keywords: ["Blog", "Website", "Notion"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
